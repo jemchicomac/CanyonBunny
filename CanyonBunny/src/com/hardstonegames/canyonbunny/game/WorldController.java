121fe8f39c85e2a452a4fd736c1bfc07aa04e5f6
ander94lakx <andergranado@outlook.com>
2014-07-02 05:34:18 +0200

    Adds input only for debugging

--- CanyonBunny/src/com/hardstonegames/canyonbunny/game/WorldController.java
+++ CanyonBunny/src/com/hardstonegames/canyonbunny/game/WorldController.java
@@ -2,24 +2,34 @@ package com.hardstonegames.canyonbunny.game;
 
 import sun.font.CreatedFontTracker;
 
+import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.Input.Keys;
 
-public class WorldController {
+import com.hardstonegames.canyonbunny.util.CameraHelper;
+
+public class WorldController extends InputAdapter{
 	
 	private static final String TAG = WorldController.class.getName(); // For debugging
 	
 	public Sprite[] testSprites;
 	public int selectedSprite;
 	
+	public CameraHelper cameraHelper;
+	
 	public WorldController() {
 		init();
 	}
 	
 	private void init() {
+		Gdx.input.setInputProcessor(this);
+		cameraHelper = new CameraHelper();
 		initTestObjects();
 	}
 	
@@ -65,9 +75,12 @@ public class WorldController {
 	}
 	
 	public void update(float deltaTime){
+		handleDebugInput(deltaTime);
 		updateTestObjects(deltaTime);
+		cameraHelper.update(deltaTime);
 	}
 	
+
 	private void updateTestObjects(float deltaTime) {
 		// Get current rotation from selected sprite
 		float rotation = testSprites[selectedSprite].getRotation();
@@ -78,4 +91,68 @@ public class WorldController {
 		// Set the rotation value to selected sprite
 		testSprites[selectedSprite].setRotation(rotation);
 	}
+	
+	private void handleDebugInput(float deltaTime) {
+		if(Gdx.app.getType() != ApplicationType.Desktop) return;
+		
+		// Selected sprite controls
+		float sprMoveSpeed = 5 * deltaTime;
+		if(Gdx.input.isKeyPressed(Keys.A)) moveSelectedSprite(-sprMoveSpeed, 0);
+		if(Gdx.input.isKeyPressed(Keys.D)) moveSelectedSprite(sprMoveSpeed, 0);
+		if(Gdx.input.isKeyPressed(Keys.W)) moveSelectedSprite(0, sprMoveSpeed);
+		if(Gdx.input.isKeyPressed(Keys.S)) moveSelectedSprite(0, -sprMoveSpeed);
+		
+		// Camera controls (move)
+		float camMoveSpeed = 5 * deltaTime;
+		float camMoveSpeedAccelerationFactor = 5;
+		if(Gdx.input.isKeyPressed(Keys.SHIFT_LEFT)) camMoveSpeed *= camMoveSpeedAccelerationFactor;
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) moveCamera(-camMoveSpeed, 0);
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)) moveCamera(camMoveSpeed, 0);
+		if(Gdx.input.isKeyPressed(Keys.UP)) moveCamera(0, camMoveSpeed);
+		if(Gdx.input.isKeyPressed(Keys.DOWN)) moveCamera(0, -camMoveSpeed);
+		if(Gdx.input.isKeyPressed(Keys.BACKSPACE)) cameraHelper.setPosition(0, 0);
+		
+		// Camera controls (zoom)
+		float camZoomSpeed = 1 * deltaTime;
+		float camZoomSpeedAccelerationFactor = 5;
+		if(Gdx.input.isKeyPressed(Keys.SHIFT_LEFT)) camZoomSpeed *= camZoomSpeedAccelerationFactor;
+		if(Gdx.input.isKeyPressed(Keys.COMMA)) cameraHelper.addZoom(camZoomSpeed);
+		if(Gdx.input.isKeyPressed(Keys.PERIOD)) cameraHelper.addZoom(-camZoomSpeed);
+		if(Gdx.input.isKeyPressed(Keys.SLASH)) cameraHelper.setZoom(1);
+	}
+	
+	private void moveSelectedSprite(float x, float y) {
+		testSprites[selectedSprite].translate(x, y);
+	}
+	
+	private void moveCamera(float x, float y) {
+		x += cameraHelper.getPosition().x;
+		y += cameraHelper.getPosition().y;
+		cameraHelper.setPosition(x, y);
+	}
+	
+	@Override
+	public boolean keyUp(int keycode) {
+		// Reset game world
+		if(keycode == Keys.R) {
+			init();
+			Gdx.app.debug(TAG, "Game world resetted");
+		}
+		// Select next sprite
+		else if(keycode == Keys.SPACE) {
+			selectedSprite = (selectedSprite + 1) % testSprites.length;
+			// Updates camera target to follow the currently selected sprite
+			if(cameraHelper.hasTarget()) {
+				cameraHelper.setTarget(testSprites[selectedSprite]);
+			}
+			Gdx.app.debug(TAG, "Sprite #" + selectedSprite + " selected");
+		}
+		else if(keycode == Keys.ENTER) {
+			cameraHelper.setTarget(cameraHelper.hasTarget() ? null : testSprites[selectedSprite]);
+			Gdx.app.debug(TAG, "Camera follow Enabled: " + cameraHelper.hasTarget());
+		}
+		return false;
+	}
+	
+
 }
