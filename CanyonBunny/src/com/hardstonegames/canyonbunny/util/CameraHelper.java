6585adf92a019fe188ec2319f1a8247a0206ef74
ander94lakx <andergranado@outlook.com>
2014-07-05 17:37:53 +0200

    Changes implementation to follow AbstractGameObjects instad Sprites

--- CanyonBunny/src/com/hardstonegames/canyonbunny/util/CameraHelper.java
+++ CanyonBunny/src/com/hardstonegames/canyonbunny/util/CameraHelper.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.MathUtils;
 import com.hardstonegames.canyonbunny.game.WorldRenderer;
+import com.hardstonegames.canyonbunny.game.objects.AbstractGameObject;
 
 public class CameraHelper {
 	
@@ -15,7 +16,7 @@ public class CameraHelper {
 	
 	private Vector2 position;
 	private float zoom;
-	private Sprite target;
+	private AbstractGameObject target;
 	
 	public CameraHelper() {
 		position = new Vector2();
@@ -25,8 +26,8 @@ public class CameraHelper {
 	public void update(float deltaTime) {
 		if(!hasTarget()) return;
 		
-		position.x = target.getX() + target.getOriginX();
-		position.y = target.getY() + target.getOriginY();
+		position.x = target.position.x + target.origin.x;
+		position.y = target.position.y + target.origin.y;
 	}
 	
 	public void setPosition(float x, float y){
@@ -43,13 +44,13 @@ public class CameraHelper {
 	
 	public float getZoom() { return zoom; }
 	
-	public void setTarget(Sprite target) { this.target = target; }
+	public void setTarget(AbstractGameObject target) { this.target = target; }
 	
-	public Sprite getTarget() { return target; }
+	public AbstractGameObject getTarget() { return target; }
 	
 	public boolean hasTarget() { return target != null; }
 	
-	public boolean hasTarget(Sprite target) { 
+	public boolean hasTarget(AbstractGameObject target) { 
 		return hasTarget() && this.target.equals(target);
 	}
 	
