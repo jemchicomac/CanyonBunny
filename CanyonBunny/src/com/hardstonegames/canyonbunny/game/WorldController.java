cf2bc2fd8b8a2bc867e18b211161c29a595f3ef0
ander94lakx <andergranado@outlook.com>
2014-07-06 00:41:42 +0200

    Add level update int the update of the controller

--- CanyonBunny/src/com/hardstonegames/canyonbunny/game/WorldController.java
+++ CanyonBunny/src/com/hardstonegames/canyonbunny/game/WorldController.java
@@ -59,6 +59,7 @@ public class WorldController extends InputAdapter{
 	
 	public void update(float deltaTime){
 		handleDebugInput(deltaTime);
+		level.update(deltaTime);
 		cameraHelper.update(deltaTime);
 	}
 	
