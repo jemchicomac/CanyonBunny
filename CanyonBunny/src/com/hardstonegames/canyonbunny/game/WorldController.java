c39dd1f7288995e2e9a181d1ccef07cd7c884bb3
ander94lakx <andergranado@outlook.com>
2014-07-06 04:22:31 +0200

    Adds all the collision detection system

--- CanyonBunny/src/com/hardstonegames/canyonbunny/game/WorldController.java
+++ CanyonBunny/src/com/hardstonegames/canyonbunny/game/WorldController.java
@@ -11,8 +11,14 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.math.Rectangle;
 import com.hardstonegames.canyonbunny.game.objects.Rock;
 import com.hardstonegames.canyonbunny.util.Constants;
+import com.hardstonegames.canyonbunny.game.objects.BunnyHead;
+import com.hardstonegames.canyonbunny.game.objects.BunnyHead.JUMP_STATE;
+import com.hardstonegames.canyonbunny.game.objects.Feather;
+import com.hardstonegames.canyonbunny.game.objects.GoldCoin;
+import com.hardstonegames.canyonbunny.game.objects.Rock;
 
 import com.hardstonegames.canyonbunny.util.CameraHelper;
 
@@ -23,6 +29,10 @@ public class WorldController extends InputAdapter{
 	public Level level;
 	public int lives;
 	public int score;
+	
+	// Rectangles for colision detection
+	private Rectangle r1 = new Rectangle();
+	private Rectangle r2 = new Rectangle();
 	
 	public CameraHelper cameraHelper;
 	
@@ -60,6 +70,7 @@ public class WorldController extends InputAdapter{
 	public void update(float deltaTime){
 		handleDebugInput(deltaTime);
 		level.update(deltaTime);
+		testCollisions();
 		cameraHelper.update(deltaTime);
 	}
 	
@@ -102,5 +113,74 @@ public class WorldController extends InputAdapter{
 		return false;
 	}
 	
+	private void onCollisionBunnyHeadWithRock(Rock rock) {
+		BunnyHead bunnyHead = level.bunnyHead;
+		float heightDifference = Math.abs(bunnyHead.position.y - (rock.position.y + rock.bounds.height));
+		if(heightDifference > 0.25f) {
+			boolean hitLeftEdge = bunnyHead.position.x > (rock.position.y + rock.bounds.width / 2.0f);
+			if(hitLeftEdge)
+				bunnyHead.position.x = rock.position.x + rock.bounds.width;
+			else
+				bunnyHead.position.x = rock.position.x - bunnyHead.bounds.width;
+			return;
+		}
+		switch (bunnyHead.jumpState) {
+			case GROUNDED:	
+				
+				break;
+				
+			case FALLING:
+			case JUMP_FALLING:
+				
+				bunnyHead.position.y = rock.position.y + bunnyHead.bounds.height + bunnyHead.origin.y;
+				bunnyHead.jumpState = JUMP_STATE.GROUNDED;
+				break;
+				
+			case JUMP_RISING:
+				
+				bunnyHead.position.y = rock.position.y + bunnyHead.bounds.height + bunnyHead.origin.y;
+				break;
+		}
+	}
+	
+	private void onCollisionBunnyWithGoldCoin(GoldCoin goldcoin) {
+		goldcoin.collected = true;
+		score += goldcoin.getScore();
+		Gdx.app.log(TAG, "Gold coin collected");
+	}
 
+	private void onCollisionBunnyWithFeather(Feather feather) {
+		feather.collected = true;
+		score += feather.getScore();
+		level.bunnyHead.setFeatherPowerup(true);
+		Gdx.app.log(TAG, "Feather collected");
+	}
+	
+	private void testCollisions() {
+		r1.set(level.bunnyHead.position.x, 
+				level.bunnyHead.position.y, 
+				level.bunnyHead.bounds.width, 
+				level.bunnyHead.bounds.height);
+		// Test collision: BunnyHead <-> Rocks
+		for(Rock rock: level.rocks) {
+			r2.set(rock.position.x, rock.position.y, rock.bounds.width, rock.bounds.height);
+			if(!r1.overlaps(r2)) continue;
+			onCollisionBunnyHeadWithRock(rock);
+			// IMPORTANT! must do all collisions for valid edge testing on rocks
+		}
+		// Test collision: BunnyHead <-> Gold Coins
+		for(GoldCoin goldCoin: level.goldcoins) {
+			r2.set(goldCoin.position.x, goldCoin.position.y, goldCoin.bounds.width, goldCoin.bounds.height);
+			if(!r1.overlaps(r2)) continue;
+			onCollisionBunnyWithGoldCoin(goldCoin);
+			break;
+		}
+		// Test collision: BunnyHead <-> Feathers
+		for(Feather feather: level.feathers) {
+			r2.set(feather.position.x, feather.position.y, feather.bounds.width, feather.bounds.height);
+			if(!r1.overlaps(r2)) continue;
+			onCollisionBunnyWithFeather(feather);
+			break;
+		}
+	}
 }
